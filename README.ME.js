0792d9625689e68e71ee27292880bdf3f75feaa9
zhengyongjie <zhengyongjie@gridsum.com>
2018-09-20 19:58:20 +0800

    update readme.me

--- README.ME.js
+++ README.ME.js
@@ -1,2 +1,27 @@
+# GenerateCode.js
+it used to produce 7 bit string, can be produced by quantity, not duplicated, can be verified.
+
 ## easy to use
-- just run: node GenerateCode if you had install node.js
+1、'cipher' you need to save, 'start' it need increase last sum value if you want to produced code in next time;
+``` 
+const generateCode = require('generateCode');
+const cipher = generateCode.noRepeatStr(); //example: DJosIOVYlEmahcr6Mf4dSCBqzNX1bPwteyGL
+const start = 123; 
+const sum = 200;
+const codeArray = generateCode.generate(cipher, start, sum);
+console.info(codeArray);
+
+```
+2、how to verified?
+
+```
+const generateCode = require('generateCode');
+const isValidate = generateCode.validate('DFda3j3', 'DJosIOVYlEmahcr6Mf4dSCBqzNX1bPwteyGL'); 
+console.info('isValidate:' + isValidate);
+
+```
+
+# end 
+The code is very simple, you are so smart and you must be understand.
+
+
